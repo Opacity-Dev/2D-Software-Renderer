a73ffc969a67d7714d0406cb40bc06d8b80d87f8
Opacity-Dev <63130334+Opacity-Dev@users.noreply.github.com>
2020-04-04 02:37:53 +0300

    GameContainer basic structure created

---
+++ SoftwareRenderer/src/com/opacity/engine/GameContainer.java
@@ -0,0 +1,23 @@
+package com.opacity.engine;
+
+public class GameContainer implements Runnable {
+	public GameContainer() {
+		
+	}
+	
+	public void start() {
+		
+	}
+	
+	public void stop() {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	private void dispose() {
+		
+	}
+}
